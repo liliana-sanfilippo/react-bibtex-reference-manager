bb45778ba3c9f6936d2776583d5c365ed7b9b679
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-08-21 17:46:37 +0200

    config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     publicDir: "demo-public",
+    optimizeDeps: {
+        include: [
+            '@liliana-sanfilippo/author-name-parser',
+            '@liliana-sanfilippo/bibtex-ts-parser'
+        ]
+    }
 });
