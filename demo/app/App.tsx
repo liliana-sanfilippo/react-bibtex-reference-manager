9acbbdcb231d520a7fbb55d22d7bc8a958721dc2
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-10-04 16:52:14 +0200

    demo

--- demo/app/App.tsx
+++ demo/app/App.tsx
@@ -3,7 +3,7 @@ import "./app.css";
 import {LoremIpsum} from 'react-lorem-ipsum';
 import {bibts} from "./examplebib";
 import {useEffect, useState} from "react";
-import {CitationlLink, CitationProvider, ManagedCitation} from "../../src";
+import {CitationlLink, CitationProvider, ManagedCitation} from "@liliana-sanfilippo/react-bibtex-reference-manager";
 
 export default function App() {
     const [data, setData] = useState<string[] | string>([]);
@@ -16,7 +16,7 @@ export default function App() {
         <div style={{maxWidth: "50%", margin: "auto"}}>
             <h1>Demo für Bibtex Citation Manager</h1>
             Here a link is set to a specific id<CitationlLink referenceID="hjhhbhjb" />.
-            Here a link is set to a specific id<CitationlLink referenceID={["hjhhbhjb", "chen_noninvasively_2021"]}/>.
+            Here a link is set to a specific id<CitationlLink referenceID={""}/>.
             Here a link is set to a specific id<CitationlLink referenceID="chen_noninvasively_2021"/>.
             Here a link is set to a specific id<CitationlLink referenceID="hjhhbhjb" />.
             <LoremIpsum p={6} avgWordsPerSentence={6} avgSentencesPerParagraph={5} />
