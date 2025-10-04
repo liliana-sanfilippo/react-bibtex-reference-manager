447575e167bebd5561b321896eecb8314039517e
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-10-04 17:17:41 +0200

    demo fix

--- demo/app/App.tsx
+++ demo/app/App.tsx
@@ -3,7 +3,7 @@ import "./app.css";
 import {LoremIpsum} from 'react-lorem-ipsum';
 import {bibts} from "./examplebib";
 import {useEffect, useState} from "react";
-import {CitationlLink, CitationProvider, ManagedCitation} from "@liliana-sanfilippo/react-bibtex-reference-manager";
+import {CitationLink, CitationProvider, ManagedCitation} from "@liliana-sanfilippo/react-bibtex-reference-manager";
 
 export default function App() {
     const [data, setData] = useState<string[] | string>([]);
@@ -15,9 +15,9 @@ export default function App() {
     <div style={{alignContent:'center'}}>
         <div style={{maxWidth: "50%", margin: "auto"}}>
             <h1>Demo für Bibtex Citation Manager</h1>
-            Here a link is set to a specific id<CitationlLink referenceID="hjhhbhjb" />.
-            Here a link is set to a specific id<CitationlLink referenceID="chen_noninvasively_2021"/>.
-            Here a link is set to a specific id<CitationlLink referenceID="hjhhbhjb" />.
+            Here a link is set to a specific id<CitationLink referenceID="hjhhbhjb" />.
+            Here a link is set to a specific id<CitationLink referenceID="chen_noninvasively_2021"/>.
+            Here a link is set to a specific id<CitationLink referenceID="hjhhbhjb" />.
             <LoremIpsum p={6} avgWordsPerSentence={6} avgSentencesPerParagraph={5} />
             <LoremIpsum p={6} avgWordsPerSentence={6} avgSentencesPerParagraph={5} />
             <ManagedCitation/>
